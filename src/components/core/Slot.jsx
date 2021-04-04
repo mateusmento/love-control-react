c286aea65260f44ac5bdd1a8b68e6ad15c823563
Mateus Sarmento <omateusmento@gmail.com>
2021-04-03 21:20:18 -0300

    Implement filtering and merging of fillings

--- src/components/core/Slot.jsx
+++ src/components/core/Slot.jsx
@@ -1,13 +1,15 @@
+import { omit } from 'lodash';
 import { Children, cloneElement, createElement, isValidElement } from 'react';
 import { callable } from '../../util';
 
 
-export function Slot({name, as, required, content, multiple, data, props: defaultProps, children: defaultChildren = []}) {
+export function Slot({name, as, required, content, multiple, filterable, merge, data, props, children = []}) {
 	content = content instanceof Array ? content : isValidElement(content) ? [content] : [];
 
 	function findAndComputeNamedFillings() {
-		let fillings = findFillingsByName(content, name, multiple)
-		return computeFillings(fillings, defaultChildren, defaultProps, data);
+		let fillings = findFillingsByName(content, name, multiple, filterable, merge);
+		fillings = fillings.length > 0 && merge ? fillings.reduce(merge) : fillings;
+		return computeFillings(fillings, children, props, data);
 	}
 
 	let fillings = name ? findAndComputeNamedFillings() : findDefaultFillings(content);
@@ -15,39 +17,43 @@ export function Slot({name, as, required, content, multiple, data, props: defaul
 	if (fillings.length > 0) return fillings;
 	if (required) return null;
 
-	if (defaultChildren instanceof Function)
-		return defaultChildren({}, null, 0);
+	if (children instanceof Function)
+		return children({}, null, 0);
 
-	return as ? createElement(as, defaultProps, defaultChildren) : defaultChildren;
+	return as ? createElement(as, props, children) : children;
 }
 
 Slot.exists = (children, name) => children.some(nameEqualsFactory(name));
-
 
 let nameEqualsFactory = name => name instanceof Function
 	? c => name(c.type, c.props, c.props.slot)
 	: c => c.props.slot === name;
 
-
-function findFillingsByName(content, name, multiple) {
+function findFillingsByName(content, name, multiple, filterable) {
 	let nameEquals = nameEqualsFactory(name);
-	let fillings = multiple
-		? content.filter(c => c.$$typeof && nameEquals(c))
-		: [content.find(c => c.$$typeof && nameEquals(c))].filter(c => c);
-	return fillings;
+	let filter = ({props: {filter}}, data) => !filter || filter(...data);
+	let filterContent = c => c.$$typeof && nameEquals(c) && (!filterable || filter(c, filterable))
+
+	if (multiple)
+		return content.filter(filterContent);
+
+	let filling = content.find(filterContent);
+	return filling ? [filling] : [];
 }
 
-function computeFillings(fillings, defaultChildren, {children, ...defaultProps} = {}, data) {
-	let computeFilling = ({props: {children, ...props}}) =>
-		defaultChildren({...props, slot: undefined}, children, fillings.length)
+function computeFillings(fillings, children, defaultProps, data) {
+	let omitProps = props => omit(props, ['slot', 'filter', 'children']);
 
-	let mergeProps = ({props}) => ({...defaultProps, ...props, slot: undefined});
-	let computeChildren = ({props}) => callable(props.children)(data);
-	let cloneFilling = (fill) => cloneElement(fill, mergeProps(fill), computeChildren(fill));
+	function cloneFilling(fill) {
+		let props = omitProps({...defaultProps, ...fill.props});
+		let children = callable(fill.props.children)(data);
+		return cloneElement(fill, props, children);
+	}
 
-	if (defaultChildren instanceof Function)
-		return Children.map(fillings, computeFilling);
-	return Children.map(fillings, cloneFilling);
+	let renderChildren = ({props}) => children(omitProps(props), props.children, fillings.length);
+
+	let map = children instanceof Function ? renderChildren : cloneFilling;
+	return Children.map(fillings, map);
 }
 
 function findDefaultFillings(content) {
