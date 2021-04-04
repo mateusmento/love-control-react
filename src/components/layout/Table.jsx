3db63d06297c1c4d46605ff3e9a840cfa5b10da7
Mateus Sarmento <omateusmento@gmail.com>
2021-04-03 21:21:39 -0300

    Merge props of Rows slots and pass item to onClick event

--- src/components/layout/Table.jsx
+++ src/components/layout/Table.jsx
@@ -8,44 +8,55 @@ export let Cell = (props) => <td {...props}/>;
 export let Rows = () => null;
 export let RowDetails = () => null;
 
+function mergeClassNames(a = '', b = '') {
+	return [a || '', b || ''].join(' ');
+}
 
+let mergeProps = (a, b) => ({
+	...a,
+	props: {
+		...a.props,
+		...b.props,
+		className: mergeClassNames(a.props.className, b.props.className)
+	}
+});
 
-export function Table({items, children}) {
+export function Table({items, children, ...props}) {
 	let columnsCount = children.filter(c => c.type === Column).length;
 
 	return (
-		<table>
+		<table {...props}>
 			<Slot required name={(t, p) => t === Column && p.title} content={children}>{ () =>
 				<thead>
-						<tr>
-							<Slot required name={t => t === Column} content={children} multiple>{
-								({title}, children) =>
-									<Slot name={t => t === Head} as="th" content={children} data={{title}}>{title}</Slot>
-							}</Slot>
-						</tr>
+					<tr>
+						<Slot required name={t => t === Column} content={children} multiple>{
+							({title}, children) =>
+								<Slot name={t => t === Head} as="th" content={children} data={{title}}>{title}</Slot>
+						}</Slot>
+					</tr>
 				</thead>
 			}</Slot>
 			<Slot required name={(t, p) => t === Column} content={children}>{ () =>
 				<tbody>{
-						items.map((item, i) =>
-							<Slot key={i} name={t => t === Rows} content={children} filterable={[item, i, items]} multiple>{
-								({filter, ...props}) => <>
-									<tr {...props}>
-										<Slot required name={t => t === Column} content={children} multiple>{
-											({field}, children) => {
-												let value = get(item, field);
-												return <Slot name={t => t === Cell} as="td" content={children} data={{value, item, index: i}}>{value}</Slot>
-											}
-										}</Slot>
-									</tr>
-
-									<Slot required name={t => t === RowDetails} content={children} filterable={[item, i, items]}>{
-										(props, children) => <tr {...props}><td colSpan={columnsCount}>{children}</td></tr>
+					items.map((item, i, items) =>
+						<Slot key={i} name={t => t === Rows} content={children} multiple filterable={[item, i, items]} merge={mergeProps}>{
+							({onClick = () => {}, ...props}) => <>
+								<tr {...props} onClick={e => onClick(item, e)}>
+									<Slot required name={t => t === Column} content={children} multiple>{
+										({field}, children) => {
+											let value = get(item, field);
+											return <Slot name={t => t === Cell} as="td" content={children} data={{value, item, index: i}}>{value}</Slot>
+										}
 									}</Slot>
-								</>
-							}</Slot>
-						)
-					}</tbody>
+								</tr>
+
+								<Slot required name={t => t === RowDetails} content={children} filterable={[item, i, items]}>{
+									(props, children) => <tr {...props}><td colSpan={columnsCount}>{children}</td></tr>
+								}</Slot>
+							</>
+						}</Slot>
+					)
+				}</tbody>
 			}</Slot>
 		</table>
 	);
